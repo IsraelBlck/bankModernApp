fdb18a7a6abe7ff32e75e6a0aa870426d7b0e4a8
Israel Onoja <Israelonoja21@gmail.com>
2023-12-17 11:56:30 +0100

    update: business component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+import Feature from "./Feature"
 
 const Business = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>
+            You do the business, <br className="sm:block hidden"/>
+             we’ll handle the money.
+          </h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            With the right credit card, you can improve your financial life by building credit, earning rewards and saving money.
+            But with hundreds of credit cards on the market.
+          </p>
+       <Button styles='mt-10' />
+      </div>
 
-export default Business
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <Feature />
+        ))}
+      </div>
+    </section>
+  )
+};
+
+export default Business;
