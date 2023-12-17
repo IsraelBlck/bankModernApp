e92a71c01dbe415394d7292f53da556d0242ec21
Israel Onoja <Israelonoja21@gmail.com>
2023-12-17 02:35:29 +0100

    update: hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () =>  (
                 </div>
             </div>
                 <h1 className='font-poppins font-semibold ss:text[72px] text-[52px] text-white ss:leading[100px] leading-[75px]'>
-                    Payment Method. 
+                    Payment Method.
                 </h1>
                 <p className={`${styles.paragraph} max-w-[430px] mt-5`}>
                     Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. 
